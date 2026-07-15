c80694fd86e7594a2e5d8ff2ab722400452a6447
Palitax <levinwerres@googlemail.com>
2026-07-15 16:30:52 +0200

    Fix SyntaxError by removing extra closing braces in content.js

--- scripts/content.js
+++ scripts/content.js
@@ -605,8 +605,6 @@ function scrapePrice(targetCondition, targetLocation, targetLanguages) {
         element: row,
         condition: foundConditionCode
       };
-        }
-      }
     }
   }
   return null;
